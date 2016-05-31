460412b9480c2f68a22fd7c37513844faf998780
Hong-She Liang <starofrainnight@gmail.com>
2016-05-31 19:50:44 +0800

    Extract python2 & python3 source compatible process module as standardalone module package

---
+++ README.rst
@@ -0,0 +1,10 @@
+pydgutils (PYthon DownGrade UTILS)
+----------------------------------------
+
+This library use for compatilbe purpose.
+
+It provided a simple method to preprocess sources to python2 syntax (if
+using python2) and do nothing while using python3.
+
+So that you could write your project in python3 syntax and install on python2
+without any changes, all jobs are done by 3to2 module which we depends on.
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from pip_bootstrap import use_pip
+use_pip()
+
+import sys
+from setuptools import setup, find_packages
+
+package_name = "pydgutils"
+
+our_packages = find_packages()
+
+long_description=(
+     open("README.rst", "r").read()
+     + "\n" +
+     open("CHANGES.rst", "r").read()
+     )
+
+our_requires = []
+
+# Only require 3to2 library on python2
+if sys.version_info[0] <= 2:
+    our_packages.append("3to2")
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    author="Hong-She Liang",
+    author_email="starofrainnight@gmail.com",
+    url="https://github.com/starofrainnight/%s" % package_name,
+    description="A library use for downgrade sources to python2 syntax during setup",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries",
+    ],
+    install_requires=our_requires,
+    packages=our_packages,
+    )
