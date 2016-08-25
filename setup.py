b1dc426f5a535403556370fd853a1863a18eff27
Hong-She Liang <starofrainnight@gmail.com>
2016-08-25 10:32:58 +0800

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if sys.version_info[0] <= 2:
 
 setup(
     name=package_name,
-    version="0.0.2",
+    version="0.0.3",
     author="Hong-She Liang",
     author_email="starofrainnight@gmail.com",
     url="https://github.com/starofrainnight/%s" % package_name,
