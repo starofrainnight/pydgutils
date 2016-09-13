18fff99b48fb522aa4b4da1882c732ff19b42cfe
Hong-She Liang <starofrainnight@gmail.com>
2016-09-14 00:47:19 +0800

    Added version_info_to_str() to convert a version_info object to str

--- pydgutils/version.py
+++ pydgutils/version.py
@@ -6,7 +6,10 @@ RL_BETA = 'beta'
 RL_CANDIDATE = 'candidate'
 RL_FINAL = 'final'
 
+def version_info_to_str(version_info):
+    return ".".join([str(a) for a in version_info[:3]])
+
 # Version info structure just like python3's sys.version_info
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 version_info = VersionInfo(0, 0, 10, RL_ALPHA, 0)
-__version__ = ".".join([str(a) for a in version_info[:3]])
+__version__ = version_info_to_str(version_info)
