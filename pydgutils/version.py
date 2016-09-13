3275c1eafe21338b76f92e020e101d4028d446e4
Hong-She Liang <starofrainnight@gmail.com>
2016-09-14 01:19:21 +0800

    Completed version info to str method
    Follow pep-440

--- pydgutils/version.py
+++ pydgutils/version.py
@@ -7,7 +7,15 @@ RL_CANDIDATE = 'candidate'
 RL_FINAL = 'final'
 
 def version_info_to_str(version_info):
-    return ".".join([str(a) for a in version_info[:3]])
+    result = ".".join([str(a) for a in version_info[:3]])
+
+    # Release level abbreviations
+    # Reference to https://www.python.org/dev/peps/pep-0440/
+    rl_abbreviations = {RL_ALPHA:'a', RL_BETA:'b', RL_CANDIDATE:'rc', RL_FINAL:''}
+    result += rl_abbreviations[version_info.releaselevel]
+    if version_info.releaselevel != RL_FINAL:
+        result += str(version_info.serial)
+    return result
 
 def read_version_file(base_dir, package_name):
     import os.path
@@ -19,5 +27,5 @@ def read_version_file(base_dir, package_name):
 
 # Version info structure just like python3's sys.version_info
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(0, 0, 10, RL_ALPHA, 0)
+version_info = VersionInfo(0, 0, 10, RL_FINAL, 0)
 __version__ = version_info_to_str(version_info)
