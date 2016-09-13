fa9fc72a3edcb5f72f456340876c28d3c3eb9155
Hong-She Liang <starofrainnight@gmail.com>
2016-09-14 01:02:36 +0800

    Added helper method read_version_file() to easily read version file

--- pydgutils/version.py
+++ pydgutils/version.py
@@ -9,6 +9,14 @@ RL_FINAL = 'final'
 def version_info_to_str(version_info):
     return ".".join([str(a) for a in version_info[:3]])
 
+def read_version_file(base_dir, package_name):
+    import os.path
+
+    package_name = package_name.replace("-", "_").replace(".", "/")
+    version_file_path = os.path.join(base_dir, package_name, "version.py")
+    with open(version_file_path) as f:
+        return f.read()
+
 # Version info structure just like python3's sys.version_info
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 version_info = VersionInfo(0, 0, 10, RL_ALPHA, 0)
