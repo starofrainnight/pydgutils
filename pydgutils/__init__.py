39a3f5123261c71870538be5951efcfab6afaed0
Hong-She Liang <starofrainnight@gmail.com>
2018-01-21 11:38:42 +0800

    Fix source code format

--- pydgutils/__init__.py
+++ pydgutils/__init__.py
@@ -17,6 +17,7 @@ from .version import __version__, version_info
 _PROCESSED_DIR = "build/downgraded_src"
 _SOURCE_DIR = "src"
 
+
 def __copy_tree(src_dir, dest_dir):
     """
     The shutil.copytree() or distutils.dir_util.copy_tree() will happen to report
@@ -39,10 +40,12 @@ def __copy_tree(src_dir, dest_dir):
         else:
             shutil.copy2(from_path, to_path)
 
+
 def remove_temporary_directories(base_dir):
     # The 'build' and 'dist' folder sometimes will not update! So we need to
     # remove them all !
-    leave_dir = os.path.realpath(os.path.normpath(os.path.join(base_dir, _PROCESSED_DIR)))
+    leave_dir = os.path.realpath(os.path.normpath(
+        os.path.join(base_dir, _PROCESSED_DIR)))
     root_dir = os.path.join(base_dir, "build")
     if not os.path.exists(root_dir):
         return
@@ -50,15 +53,17 @@ def remove_temporary_directories(base_dir):
     files = os.listdir(root_dir)
     is_leave_dir_existed = os.path.exists(leave_dir)
     for afile in files:
-        afile_path = os.path.realpath(os.path.normpath(os.path.join(root_dir, afile)))
+        afile_path = os.path.realpath(
+            os.path.normpath(os.path.join(root_dir, afile)))
         if os.path.isdir(afile_path):
             if ((not is_leave_dir_existed)
-                or (not os.path.samefile(afile_path, leave_dir))):
+                    or (not os.path.samefile(afile_path, leave_dir))):
                 shutil.rmtree(afile_path, ignore_errors=True)
         else:
             os.remove(afile_path)
 
     shutil.rmtree(os.path.join(base_dir, 'dist'), ignore_errors=True)
+
 
 def downgrade_files(files):
     # Check and prepare 3to2 module.
@@ -83,16 +88,18 @@ def downgrade_files(files):
             "-w",
             "-n",
             "--no-diffs",
-            ]
+        ]
         options += files
 
         lib3to2_main("lib3to2.fixes", options)
+
 
 def is_path_in_filters(apath, filters):
     for afilter in filters:
         if fnmatch.fnmatch(apath, afilter):
             return True
     return False
+
 
 def process(base_dir=os.curdir):
     """
@@ -220,6 +227,7 @@ def process(base_dir=os.curdir):
 
     return destination_path
 
+
 def process_packages(base_dir=os.curdir):
     """
     Search local packages and downgrade it if needs, then return founded
@@ -231,6 +239,7 @@ def process_packages(base_dir=os.curdir):
     packages = find_packages(where=source_dir)
     return (packages, source_dir)
 
+
 def process_requirements():
     """
     Simple function to get all requirements from ./requirements.txt, so we need
@@ -239,7 +248,8 @@ def process_requirements():
 
     from pip.req import parse_requirements
 
-    # parse_requirements() returns generator of pip.req.InstallRequirement objects
+    # parse_requirements() returns generator of pip.req.InstallRequirement
+    # objects
     requirements = parse_requirements("./requirements.txt", session=False)
     requirements = [str(ir.req) for ir in requirements]
 
