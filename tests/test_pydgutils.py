a7c7638ce1725d2de7cf6697f94342362825f88f
Hong-She Liang <starofrainnight@gmail.com>
2018-01-21 12:25:36 +0800

    Added basic tests files

---
+++ tests/test_pydgutils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `pydgutils` package."""
+
+import pydgutils
+import os.path
+
+
+def test_pydgutils():
+    """
+    Test if pydgutils
+    """
+
+    assert True
