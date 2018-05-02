b6639963c325dd60ddee52c188bd3dd34236cb72
Hong-She Liang <starofrainnight@gmail.com>
2018-05-02 14:03:12 +0800

    Update copyright's date

--- pydgutils_bootstrap.py
+++ pydgutils_bootstrap.py
@@ -1,7 +1,7 @@
 '''
 The MIT License (MIT)
 
-Copyright (c) 2016, Hong-She Liang <starofrainnight@gmail.com>.
+Copyright (c) 2016-2018, Hong-She Liang <starofrainnight@gmail.com>.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
