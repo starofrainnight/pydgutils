0d03f1447517b48fce990f2d15907c7de2a6a979
Hong-She Liang <starofrainnight@gmail.com>
2018-05-02 12:45:37 +0800

    Bump version: 0.0.14 → 0.0.15

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ shutil.rmtree(os.path.join(os.curdir, 'build'), ignore_errors=True)
 
 setup(
     name=package_name,
-    version='0.0.14',
+    version='0.0.15',
     author="Hong-She Liang",
     author_email="starofrainnight@gmail.com",
     url="https://github.com/starofrainnight/%s" % package_name,
